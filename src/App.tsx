7948ef375ad47976d609939fb8ca42c2da7b2e96
allyson7 <allysonlopes0@gmail.com>
2022-11-17 11:03:03 -0400

    :sparkles: finished lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Envelope } from "phosphor-react";
+import { Envelope, Lock } from "phosphor-react";
+import { Button } from "./components/Button";
+import { Checkbox } from "./components/Checkbox";
 import { Heading } from "./components/Heading";
 import { Text } from "./components/Text";
 import { TextInput } from "./components/TextInput";
@@ -18,16 +20,62 @@ export function App() {
         </Text>
       </header>
 
-      <form className="flex flex-col items-stretch w-full max-w-[400px]">
+      <form className="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
         <label htmlFor="email" className="flex flex-col gap-3">
           <Text className="font-semibold">Endereço de email</Text>
           <TextInput.Root>
             <TextInput.Icon>
               <Envelope />
             </TextInput.Icon>
-            <TextInput.Input placeholder="Digite seu email" />
+            <TextInput.Input
+              type="email"
+              id="email"
+              placeholder="Digite seu email"
+            />
           </TextInput.Root>
         </label>
+
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input
+              type="password"
+              id="password"
+              placeholder="*******"
+            />
+          </TextInput.Root>
+        </label>
+
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm" className="text-gray-200">
+            Lembrar de mim por 30 dias
+          </Text>
+        </label>
+
+        <Button type="submit" className="mt-4">
+          Entrar na plataforma
+        </Button>
+
+        <footer className="flex flex-col items-center gap-4 mt-8">
+          <Text
+            size="sm"
+            className="text-gray-400 underline hover:text-gray-200 transition-colors"
+            asChild
+          >
+            <a href="">Esqueceu sua senha</a>
+          </Text>
+          <Text
+            size="sm"
+            className="text-gray-400 underline hover:text-gray-200 transition-colors"
+            asChild
+          >
+            <a href="">Não possui conta? Crie uma agora</a>
+          </Text>
+        </footer>
       </form>
     </div>
   );
